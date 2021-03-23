d611dbaac78a985376ad604ca919f44d46cc5938
Joshua Miller <49739062+DigitalJEM@users.noreply.github.com>
2021-03-22 17:56:06 -0700

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -110,7 +110,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "ALCnudgeLess" not in self.config:
-        self.config.update({"ALCnudgeLess":"1"})
+        self.config.update({"ALCnudgeLess":"0"})
         self.config.update({"ALCminSpeed":"13.4112"})
         self.element_updated = True
 
@@ -138,13 +138,25 @@ class kegman_conf():
         self.config.update({"AutoHold":"1"})
         self.element_updated = True
 	
+      if "cameraOffset" not in self.config:
+	    self.config.update({"cameraOffset":"0.05"})
+	    self.element_updated = True
+
+	  if "wheelTouchSeconds" not in self.config:
+	  	self.config.update({"wheelTouchSeconds":"600"})
+	  	self.element_updated = True
+	  	
+	  if "STOPPING_DISTANCE" not in self.config:
+	  	self.config.update({"STOPPING_DISTANCE":"2.5"})
+	  	self.element_updated = True
+	  		
       if self.element_updated:
         print("updated")
         self.write_config(self.config)
 
     else:
-      self.config = {"cameraOffset":"0.05", "lastTrMode":"1", "battChargeMin":"75", "battChargeMax":"90", \
-                     "wheelTouchSeconds":"180", "accelerationMode":"1","battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
+      self.config = {"cameraOffset":"0.05", "lastTrMode":"1", "battChargeMin":"90", "battChargeMax":"70", \
+                     "wheelTouchSeconds":"600", "accelerationMode":"1","battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
                      "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"1",\
                      "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"0.0", \
                      "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
@@ -152,8 +164,8 @@ class kegman_conf():
                      "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
                      "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", \
                      "sR_boost":"0", "sR_BP0":"0", "sR_BP1":"0", "sR_time":"1", \
-                     "ALCnudgeLess":"1", "ALCminSpeed":"13.4112", "ALCtimer":"2.0", "CruiseDelta":"8", \
-                     "CruiseEnableMin":"32", "epsModded": "0", "STOPPING_DISTANCE":"2.5"}
+                     "ALCnudgeLess":"0", "ALCminSpeed":"13.4112", "ALCtimer":"2.0", "CruiseDelta":"8", \
+                     "CruiseEnableMin":"32", "epsModded":"0", "STOPPING_DISTANCE":"2.5"}
 
 
       self.write_config(self.config)
