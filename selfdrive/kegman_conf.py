b927884f86a2dea5f6b9a7c2f54494c3e37b865f
Joshua Miller <49739062+DigitalJEM@users.noreply.github.com>
2021-03-27 15:47:45 -0700

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -44,7 +44,7 @@ class kegman_conf():
         self.config = json.load(f)
 
       if "battPercOff" not in self.config:
-        self.config.update({"battPercOff":"25"})
+        self.config.update({"battPercOff":"15"})
         self.config.update({"carVoltageMinEonShutdown":"11800"})
         self.config.update({"brakeStoppingTarget":"0.25"})
         self.element_updated = True
@@ -110,8 +110,8 @@ class kegman_conf():
         self.element_updated = True
 
       if "ALCnudgeLess" not in self.config:
-        self.config.update({"ALCnudgeLess":"0"})
-        self.config.update({"ALCminSpeed":"20.1168"})
+        self.config.update({"ALCnudgeLess":"1"})
+        self.config.update({"ALCminSpeed":"13.4112"})
         self.element_updated = True
 
       if "ALCtimer" not in self.config:
@@ -123,7 +123,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "CruiseEnableMin" not in self.config:
-        self.config.update({"CruiseEnableMin":"40"})
+        self.config.update({"CruiseEnableMin":"32"})
         self.element_updated = True
 
       if "epsModded" not in self.config:
@@ -135,7 +135,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "AutoHold" not in self.config:
-        self.config.update({"AutoHold":"0"})
+        self.config.update({"AutoHold":"1"})
         self.element_updated = True
 	
       if self.element_updated:
@@ -143,17 +143,17 @@ class kegman_conf():
         self.write_config(self.config)
 
     else:
-      self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
-                     "wheelTouchSeconds":"180", "accelerationMode":"1","battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
-                     "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"0",\
+      self.config = {"cameraOffset":"0.05", "lastTrMode":"1", "battChargeMin":"99", "battChargeMax":"100", \
+                     "wheelTouchSeconds":"600", "accelerationMode":"1","battPercOff":"15", "carVoltageMinEonShutdown":"11800", \
+                     "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"1",\
                      "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"0.0", \
                      "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
                      "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
                      "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
                      "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", \
                      "sR_boost":"0", "sR_BP0":"0", "sR_BP1":"0", "sR_time":"1", \
-                     "ALCnudgeLess":"0", "ALCminSpeed":"20.1168", "ALCtimer":"2.0", "CruiseDelta":"8", \
-                     "CruiseEnableMin":"40", "epsModded": "0"}
+                     "ALCnudgeLess":"1", "ALCminSpeed":"20.1168", "ALCtimer":"2.0", "CruiseDelta":"8", \
+                     "CruiseEnableMin":"32", "epsModded": "0"}
 
 
       self.write_config(self.config)
