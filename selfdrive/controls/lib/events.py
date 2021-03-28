cc2bcada7125341943aaa10a13a1d864593eb88a
Joshua Miller <49739062+DigitalJEM@users.noreply.github.com>
2021-03-27 18:44:43 -0700

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -255,8 +255,8 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
 
   EventName.startupGreyPanda: {
     ET.PERMANENT: Alert(
-      "WARNING: Grey panda is not supported.",
-      "But Kegman re-enabled it.  Enjoy.",
+      "Welcome to AutoCopilot",
+      "v0.8.1 - Grey Panda edition",
       AlertStatus.userPrompt, AlertSize.mid,
       Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 5.),
   },
